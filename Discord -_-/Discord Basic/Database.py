287142a1eb3115013b9d866ea63422919184014e
Deniz bro bro <140730727+Denizbrobro@users.noreply.github.com>
2023-09-06 12:01:48 +0300

    Database.py

--- Discord -_-/Discord Basic/Database.py
+++ Discord -_-/Discord Basic/Database.py
@@ -5,34 +5,34 @@ bot = commands.Bot(command_prefix='é', intents=discord.Intents.all())
 
 @bot.event
 async def on_ready():
-    print("Bot Aktif")
+     print("Bot Active")
 
 
 @bot.event
 async def on_message(message: discord.Message):
-    print("Kanal » ",message.channel,"|||",message.author," » ",message.content)
-    print("                                                                    ")
+     print("Channel » ",message.channel,"|||",message.author," » ",message.content)
+     print(" ")
 
 
 @bot.event
 async def on_member_join(member: discord.Member):
-    print("Herhangi Bir Sunucuya » ",member.name," Giriş Yaptı")
-    print("                                                    ")
+     print("Logged in to Any Server » ",member.name," Login")
+     print(" ")
 
 @bot.event
 async def on_member_remove(member: discord.Member):
-    print("Herhangi Bir Sunucuya » ",member.name," Çıkış Yaptı")
-    print("                                                    ")
+     print("To Any Server » ",member.name," Signed Out")
+     print(" ")
 
 @bot.event
 async def on_guild_channel_create(channel: discord.TextChannel):
-    print("Herhangi Bir Sunucuda » ",channel.name,"| Diye Bir Kanal Oluşturuldu.")
-    print("                                                    ")
+     print("A Channel Has Been Created On Any Server » ",channel.name,"|")
+     print(" ")
 
 @bot.event
 async def on_guild_channel_delete(channel: discord.TextChannel):
-    print("Herhangi Bir Sunucuda » ",channel.name,"| Diye Bir Kanal Silindi.")
-    print("                                                    ")
+     print("On Any Server » ",channel.name,"| A Channel Has Been Deleted.")
+     print(" ")
 
 
 bot.run("Your Token Here")
