d9ec89f92314c23fe93e291fbc58efe4197bdaaa
Deniz bro bro <140730727+Denizbrobro@users.noreply.github.com>
2023-09-06 12:32:46 +0300

    commands.py

--- Discord -_-/Discord Basic/commands.py
+++ Discord -_-/Discord Basic/commands.py
@@ -15,7 +15,7 @@ async def hello(ctx): #there are commands and variables here and I must say ctx 
 @bot.command() #here @bot.command this is required if you are making commands
 async def say(ctx, *, message): #Here you see a new variable, the function of this variable is capital and asterisk
      await ctx.send(message) #the reason for the star here If there was no star, it would only send 1 word, but because it was a star, it sent them all
-     print("!count used.")
+     print("!say used.")
 
 @bot.command()
 async def bye(ctx): #Here I Taught You How to Delete Messages
